168f67c68ce773741fe30bd4f825b3b428d32542
Aqua Man <stepanet@gmail.com>
2026-09-21 22:22:21 +0300

    Добавили утилиту расчетов

---
+++ CarTracker/ViewModels/ReminderCalculator.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderCalculator.swift
+//  CarTracker
+//
+//  Created by stepanet on 21.09.2026.
+//
+
+import Foundation
---
+++ CarTracker/ViewModels/ReminderStore.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderStore.swift
+//  CarTracker
+//
+//  Created by stepanet on 21.09.2026.
+//
+
+import Foundation
